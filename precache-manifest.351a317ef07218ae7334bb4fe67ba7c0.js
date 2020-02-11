665ae4f9edc5e76ffc577be0b663c56366146530
Monika Ludwikowska <monikaagaludwikowska79@gmail.com>
2020-02-11 19:51:52 +0100

    Updates

---
+++ precache-manifest.351a317ef07218ae7334bb4fe67ba7c0.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7bf5b5d57f5059230bc2af4d486bf1ed",
+    "url": "/app-rr/index.html"
+  },
+  {
+    "revision": "756af676987baa260b54",
+    "url": "/app-rr/static/css/main.4b64c2d7.chunk.css"
+  },
+  {
+    "revision": "44804ccf52fbf3d319a2",
+    "url": "/app-rr/static/js/2.0cfa552b.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/app-rr/static/js/2.0cfa552b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "756af676987baa260b54",
+    "url": "/app-rr/static/js/main.fb73e09a.chunk.js"
+  },
+  {
+    "revision": "8119b65e75202ebcb550",
+    "url": "/app-rr/static/js/runtime-main.981fa7a9.js"
+  },
+  {
+    "revision": "a2e5798d8a4956625b10b7b30dcecd6c",
+    "url": "/app-rr/static/media/header1.a2e5798d.jpg"
+  },
+  {
+    "revision": "89ea284c1031a75173b28092b1675003",
+    "url": "/app-rr/static/media/header2.89ea284c.jpg"
+  },
+  {
+    "revision": "f9a21670b51d31f90d4ecb82e40e893f",
+    "url": "/app-rr/static/media/header3.f9a21670.jpg"
+  }
+]);
